5c53d2346db64d886fb7c6bc9b6026dd8d36e78c
Pedro Domingues <senac.sp.pedro.domingues@gmail.com>
2023-09-13 07:07:13 -0300

    User Entity

---
+++ src/main/java/com/projetotrasacoesbancarias/domain/user/UserType.java
@@ -0,0 +1,8 @@
+package com.projetotrasacoesbancarias.domain.user;
+
+public enum UserType {
+
+	COMMOM,
+	MERCHANT
+	
+}
---
+++ src/test/java/com/projetotrasacoesbancarias/domain/user/UserTest.java
@@ -0,0 +1,14 @@
+package com.projetotrasacoesbancarias.domain.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
