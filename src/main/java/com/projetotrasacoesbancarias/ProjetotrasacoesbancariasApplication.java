eb9a851dda9be9088b2a33850f6d58e2eaba54d3
Pedro Domingues <senac.sp.pedro.domingues@gmail.com>
2023-09-13 06:48:58 -0300

    Initial Configuration

---
+++ src/main/java/com/projetotrasacoesbancarias/ProjetotrasacoesbancariasApplication.java
@@ -0,0 +1,13 @@
+package com.projetotrasacoesbancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotrasacoesbancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotrasacoesbancariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetotrasacoesbancarias/ProjetotrasacoesbancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetotrasacoesbancarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotrasacoesbancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
