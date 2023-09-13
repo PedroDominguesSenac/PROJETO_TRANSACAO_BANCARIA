d13461c50add41b483899b1d81d19227799d9417
Pedro Domingues <senac.sp.pedro.domingues@gmail.com>
2023-09-13 10:02:01 -0300

    User Service

---
+++ src/main/java/com/projetotrasacoesbancarias/services/UserService.java
@@ -0,0 +1,25 @@
+package com.projetotrasacoesbancarias.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.projetotrasacoesbancarias.domain.user.User;
+import com.projetotrasacoesbancarias.domain.user.UserType;
+import com.projetotrasacoesbancarias.repositories.UserRepository;
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public void validateTransaction(User sender, UserType amount) throws Exception {
+		if(sender.getUserType() == UserType.MERCHANT) {
+			throw new Exception("Comerciante nao pode realizar transacao!");
+		}
+		
+		if(sender.getBalance().compareTo(amount) < 0) {
+			throw new Exception("Saldo Insuficiente");
+		}
+	}
+}
