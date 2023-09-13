6c382afe83fa486369b08cccf5996caf02e84c2f
Pedro Domingues <senac.sp.pedro.domingues@gmail.com>
2023-09-13 08:50:03 -0300

    Transaction Repository

--- src/main/java/com/projetotrasacoesbancarias/repositories/TransactionRepository.java
+++ src/main/java/com/projetotrasacoesbancarias/repositories/TransactionRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.projetotrasacoesbancarias.domain.transaction.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> { 
-	
 
 }
